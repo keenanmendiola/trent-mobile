c8e5912e519207b66694ff4de5612d646fca046c
Keenan <keenanpaulmendiola@gmail.com>
2021-11-20 15:03:04 +0800

    Update minSdkVersion to 20 and add sample map for Map Screen

--- lib/screens/MapScreen.dart
+++ lib/screens/MapScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MapScreen extends StatefulWidget {
   static String routeName = "/map";
@@ -9,9 +10,32 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Map Here"),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: GestureDetector(
+            onTap: () {
+              Get.back();
+            },
+            child: Icon(
+              Icons.chevron_left,
+              color: Theme.of(context).accentColor,
+              size: 40,
+            ),
+          ),
+        ),
+        extendBodyBehindAppBar: true,
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/map.PNG"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: null /* add child content here */,
+        ),
       ),
     );
   }
